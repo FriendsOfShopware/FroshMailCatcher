996fa88482d5206bcbd48f90192096c66c0fdece
Soner Sayakci <s.sayakci@gmail.com>
2017-07-07 21:13:58 +0200

    Cleanup

--- Subscriber/BackendSubscriber.php
+++ Subscriber/BackendSubscriber.php
@@ -4,35 +4,25 @@ namespace ShyimMailCatcher\Subscriber;
 
 use Enlight\Event\SubscriberInterface;
 
+/**
+ * Class BackendSubscriber
+ * @package ShyimMailCatcher\Subscriber
+ */
 class BackendSubscriber implements SubscriberInterface
 {
     /**
-     * Returns an array of event names this subscriber wants to listen to.
-     *
-     * The array keys are event names and the value can be:
-     *
-     *  * The method name to call (position defaults to 0)
-     *  * An array composed of the method name to call and the priority
-     *  * An array of arrays composed of the method names to call and respective
-     *    priorities, or 0 if unset
-     *
-     * For instance:
-     *
-     * <code>
-     * return array(
-     *     'eventName0' => 'callback0',
-     *     'eventName1' => array('callback1'),
-     *     'eventName2' => array('callback2', 10),
-     *     'eventName3' => array(
-     *         array('callback3_0', 5),
-     *         array('callback3_1'),
-     *         array('callback3_2')
-     *     )
-     * );
-     *
-     * </code>
-     *
-     * @return array The event names to listen to
+     * @var string
+     */
+    private $viewDir;
+
+    public function __construct($viewDir)
+    {
+        $this->viewDir = $viewDir;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return array
      */
     public static function getSubscribedEvents()
     {
@@ -41,13 +31,16 @@ class BackendSubscriber implements SubscriberInterface
         ];
     }
 
+    /**
+     * @param \Enlight_Event_EventArgs $args
+     */
     public function onBackendIndex(\Enlight_Event_EventArgs $args)
     {
         /** @var \Shopware_Controllers_Backend_Index $subject */
         $subject = $args->getSubject();
 
         if ($subject->Request()->getActionName() === 'index') {
-            $subject->View()->addTemplateDir(Shopware()->Container()->getParameter('shyim_mail_catcher.view_dir'));
+            $subject->View()->addTemplateDir($this->viewDir);
             $subject->View()->extendsTemplate('backend/index/notifications.js');
         }
     }
